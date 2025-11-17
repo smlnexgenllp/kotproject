19ce2b71140947f9a67d12d4971e8c357aa891ed
smlnexgenllp <smlnexgenllp@gmail.com>
2025-11-17 16:51:41 +0530

    added the authentication to access the page

--- kot-management-system/frontend/src/App.jsx
+++ kot-management-system/frontend/src/App.jsx
@@ -108,7 +108,7 @@ function App() {
       <Route
         path="/cashier-wait"
         element={
-          <ProtectedRoute allowedRoles={["cashier", "admin"]}>
+          <ProtectedRoute allowedRoles={["waiter"]}>
             <CashierWait />
           </ProtectedRoute>
         }
